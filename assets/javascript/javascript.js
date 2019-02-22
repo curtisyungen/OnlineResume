ce3701144e7e189a4508f1dc9a018854784f8b8e
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-02-22 12:32:11 -0800

    Add files via upload

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,2 +1,29 @@
+$(".showMore").on("click", showMore);
 
+function showMore() {
+
+    // Get status attribute from clicked link
+    var status = $(this).attr("data-status");
+
+    // Get label name to which this link pertains
+    var label = $(this).attr("data-label");
+
+    // Update status and text of link
+    if (status == "more") {
+
+        $(`#${label}`).show();
+
+        $(this)
+        .attr("data-status", "less")
+        .text("Show less");
+    }
+    else {
+
+        $(`#${label}`).hide();
+
+        $(this)
+        .attr("data-status", "more")
+        .text("Show more");
+    }
+}
 
