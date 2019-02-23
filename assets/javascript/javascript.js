253b5139523aa0a436200f9f5335679cffb87875
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-02-22 18:00:43 -0800

    Add files via upload

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -36,3 +36,21 @@ function showMore() {
     }
 }
 
+$("#profileImg").on("click", flyJets);
+
+function flyJets() {
+
+    for (var j=0; j<6; j++) {
+        var jet = $("<p>").addClass("fas fa-fighter-jet blueAngel");
+        $("#blueAngels").append(jet);
+        $("#blueAngels").append("<br>");
+    }
+
+    $("#blueAngels").animate({ 
+        marginLeft: "+=1600px",
+    }, 4000)
+        
+    setTimeout(function() { $("#blueAngels").empty(); }, 4000);
+    
+}
+
