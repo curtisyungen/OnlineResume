8fc5a970641dd191a9dda6e8e789b93968d98e4b
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-05-10 23:37:23 -0700

    Add files via upload

---
+++ assets/javascript/navbar.js
@@ -0,0 +1,42 @@
+// NAVBAR 
+// =============================================
+
+$(window).on('scroll', function() {
+    
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = 750;  
+    
+    var $navbar = $("#navbar");
+
+    if (y_scroll_pos > scroll_pos_test) {
+        $navbar
+            .css("top", "50px")
+            .css("position", "fixed");        
+    }
+    else {
+        $navbar
+            .css("top", "auto")
+            .css("position", "absolute");
+    }
+});
+
+$(".navbar-item").on("click", function() {
+    var title = $(this).attr("data-title");
+    var scroll = 0;
+
+    console.log(title);
+
+    switch (title) {
+        case "about": scroll = 1100; break;
+        case "skills": scroll = 1400; break;
+        case "projects": scroll = 1700; break;
+        case "experience": scroll = 2300; break;
+        case "education": scroll = 2650; break;
+        case "affiliations": scroll = 2850; break;
+        case "contact": scroll = 3200; break;
+        default: "";
+    }
+
+    $("html, body").animate({ scrollTop: scroll }, 500);
+})
+
