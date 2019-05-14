b0c513a64b59b91607c36491b4d893cfc7f5c578
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-05-14 07:41:04 -0700

    Add files via upload

--- assets/javascript/info-boxes.js
+++ assets/javascript/info-boxes.js
@@ -169,6 +169,8 @@ function hideOpenBox() {
             opacity: 0
         }, 100);
 
+        $(`#${openBoxId}`).hide();
+
         // Reset the arrow icon of the open box
         $(openBox).children(".arrow")
             .removeClass("fa-arrow-circle-right")
