1ec2eed072b2a22cea4900e5ed7010a1ea15bc48
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-27 14:49:35 -0700

    update juggling animation

--- assets/javascript/funFacts.js
+++ assets/javascript/funFacts.js
@@ -17,7 +17,21 @@ $(".factRunning").hover(
 // ============================================
 $(".factJuggling").hover(
     function() {
-
+        for (var i=1; i<4; i++) {
+            $(`#ball${i}`)
+                .css("transition", "0.75s")
+                .css("margin-left", `${i * -20}px`);
+        }
+        for (var i=4; i<8; i++) {
+            $(`#ball${i}`)
+                .css("transition", "1s")
+                .css("margin-left", `${(i-4) * 20}px`);
+        }
+    },
+    function() {
+        for (var i=1; i<8; i++) {
+            $(`#ball${i}`).css("margin-left", `0px`);
+        }
     }
 );
 
